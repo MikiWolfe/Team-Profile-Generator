c4de66ed12ea619f18b720429df3a607a5942756
Mikayla Bruce <mkla.w.b@gmail.com>
2022-01-12 11:47:50 -0800

    html and css planned out

---
+++ index.js
@@ -0,0 +1,42 @@
+const employees = [];
+// manager, engineer, interns 
+
+
+// action needed:
+
+// ask them for MANAGER info 
+
+    // prompt the user for data
+
+        // THEN creat and store an object for the manager
+
+        // THEN "ask what they would like to do next"
+
+// ask them for ENGINEER info
+
+    // prompt the user for the data 
+    
+        // THEN create and store an object for the engineer
+
+        // THEN "ask what they would like to do next"
+
+// ask them for INTERN info
+
+    // prompt the user for the data 
+    
+        // THEN create and store an object for the intern
+
+        // THEN "ask what they would like to do next"
+
+// Ask that they would like to do next 
+
+    // Add engineer, interen, be done
+
+        // if 'Add Engineer' -> 'Ask them for engineer info'
+
+        // if 'Add Intern' -> 'Ask them for intern info'
+
+        // if 'Be done' -> 'build an html page'
+
+// Use all of the collected employee data to build an HTML page.
+
